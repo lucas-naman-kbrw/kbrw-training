2894b01f1fd1417fddecd2b93b16b3873ef35cf4
Lucas Naman <lucas.naman@kbrwadventure.com>
2021-03-26 11:04:06 +0100

    chap8 seup

--- chap8/chap_four/lib/database/serv_supervisor.ex
+++ chap8/chap_four/lib/database/serv_supervisor.ex
@@ -2,7 +2,7 @@ defmodule Server.Supervisor do
   import Supervisor.Spec
 
   def start_link do
-    {:ok, _} = Supervisor.start_link(__MODULE__, [order_id], name: __MODULE__)
+    {:ok, _} = Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_) do
